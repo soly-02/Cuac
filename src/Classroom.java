194fbc1d227bba2911463e6d67d5c1ff95650000
LAPTOP-GOA3VI7R\asyme <ics21158@uom.edu.gr>
2022-05-03 23:03:53 +0300

    jiiiinjk

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class Classroom {
-
+	public ArrayList<Seats> seats;
+	ArrayList<Integer> seatsArrount;
+	
+	public Classroom() {
+		this.seats = new ArrayList<Seats>();
+		this.seatsArrount = new ArrayList<Integer>();
+	}
+	
+	
 }
--- src/Seats.java
+++ src/Seats.java
@@ -1,4 +1,56 @@
+import java.sql.Time;
+import java.util.Date;
 
 public class Seats {
+	private Integer seatNum;
+	private Date date;
+	private Time time;
+	private Classroom classroom;
 
+	
+	
+	public Integer getSeatNum() {
+		return seatNum;
+	}
+
+
+
+	public void setSeatNum(Integer seatNum) {
+		this.seatNum = seatNum;
+	}
+
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+
+	public Time getTime() {
+		return time;
+	}
+
+
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+
+
+
+	public Seats(Integer seatNum, Date date, Time time, Classroom classroom) {
+		this.seatNum = seatNum;
+		this.date = date;
+		this.time = time;
+		this.classroom = classroom;
+	}
+	
+	
 }
