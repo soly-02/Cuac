cd44747b447c422435477aae474c19ca56780590
Vaggelis <ics21107@uom.edu.gr>
2022-05-05 13:54:34 +0300

    Νεες μεθοδοι send και connect. String parsing στο Registry

--- src/Registry.java
+++ src/Registry.java
@@ -5,19 +5,27 @@ import java.util.Scanner;
 
 public class Registry {
 	// Auth h klash tha diaxeirizetai ta logins, registers, logs kai ola ta arxeia tou server
-	
+	private String[] MessageArray;
+	private String[] dataFromFile;
+	private String email;
+	private String password;
+	private String pathToPDF;
+	 
 	// missing constructor
 	
-	public boolean login(String clientQuery) throws IOException {
+	public boolean login(String clientMsg) throws IOException {
 		FileInputStream inputStream = null;
 		Scanner sc = null;
+		MessageArray = clientMsg.split(", ");
+		email = MessageArray[0];
+		password = MessageArray[1];
 		try {
 		    inputStream = new FileInputStream("C:\\Users\\Vaggelis\\eclipse-workspace\\Couak\\src\\userData.txt"); //<----- absolute path name to work. Allakste to sto diko sas path
 		    sc = new Scanner(inputStream, "UTF-8");
 		    while (sc.hasNextLine()) {
 		        String line = sc.nextLine();
-		        
-		        if(line.equals(clientQuery)) {
+		        dataFromFile = line.split(", ");
+		        if((dataFromFile[0].equals(email)) && (dataFromFile[1].equals(password))) {
 		        	return true;
 		        }
 		        //System.out.println(line);
